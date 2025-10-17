8539bd7439b26f3f14310e47ed35708a74a02313
yuema137 <3124558229@qq.com>
2025-10-16 20:32:51 -0700

    add auto sim scripts

---
+++ LXePETSim/PET_example/auto_run_radius_sim.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Loop over source distances from 1.0 to 12.0 cm
+for dist in $(seq 0.0 1.0 15.0)
+do
+  echo "Running simulation for source_dist=${dist} cm"
+  python pet_sim_philips.py --source_dist ${dist}
+done
